b65ff40e30f8af6a8efab2a719af742e4bf6f258
Jostik <161108758+Agro4221@users.noreply.github.com>
2026-09-21 11:52:08 +0600

    Remove redundant launcher or duplicate documentation

--- start_bot_minimized.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-start "Stream Voice Bot" /min powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_windows.ps1"
-start "" powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0scripts\open_admin.ps1"
